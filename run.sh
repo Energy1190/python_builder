58d260174f2b0b50e7e09a2a8e096e30e6f500f7
Energy1190 <energyneo0@gmail.com>
2017-10-06 15:36:43 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ import time
 import shutil
 import docker
 import traceback
+from io import BytesIO
+from docker import APIClient
+
 
 def permission():
     os.system('chmod 777 /data/build.complate')
@@ -55,15 +58,22 @@ def fail(fail_log):
 
 
 def build_and_push(user, paswd, name, tag, version='1.23'):
+    def low_level_build(IMAGE, tag, version):
+        x = APIClient(base_url='unix://tmp/run/docker.sock', version=version)
+        response = [line for line in x.build(path='/data', rm=True, tag='{}:{}'.format(IMAGE, tag))]
+        return response
+
     log = open('/build.log', 'w')
     IMAGE = user + '/' + name
     dc = docker.DockerClient(base_url='unix://tmp/run/docker.sock', version=version)
     try:
-        x = dc.images.build(path='/data', tag='{}:{}'.format(IMAGE, tag))
+        #        x = dc.images.build(path='/data', tag='{}:{}'.format(IMAGE, tag))
+        x = list(map(str, low_level_build(IMAGE, tag, version)))
+        list(map(log.write, x))
     except docker.errors.BuildError as e:
         log.write(str(traceback.format_exc()))
         return False
-    
+
     for i in dc.images.push(IMAGE, stream=True, tag=tag, auth_config={'username': user, 'password': paswd}):
         log.write(str(i))
     log.close()
@@ -73,7 +83,7 @@ def build_and_push(user, paswd, name, tag, version='1.23'):
 def main():
     if not os.path.exists('/data/build.wait'): return False
     if not os.path.exists('/data/build.env'): return False
-    
+
     print('')
     print('Start build image.')
 
@@ -98,9 +108,11 @@ def main():
     clen_dir()
     shutil.move('/build.log', '/data/build.log')
 
-    if not x: status('1')
-    else: status('0')
-    
+    if not x:
+        status('1')
+    else:
+        status('0')
+
     complate()
 
 
